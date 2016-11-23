4b1ace6db1675add6f72e523bbea5f128af1f484
k-i-k-ichi <k-i-k-ichi@users.noreply.github.com>
2016-11-23 20:01:27 +0000

    a

--- gz01_cw2/ncsdns.py
+++ gz01_cw2/ncsdns.py
@@ -131,15 +131,12 @@ while 1:
   question = QE.fromData(data, len(header_binary))
   print header
   print question
-  print len(header)
-  print len(question)
-  rr_offset = len(header) + len(question)
-  rr = RR.fromData(data, rr_offset)
 
+  data = header.pack() + question.pack()
   (rr, rr_length,) = RR.fromData(data,k)
   cs.sendto(data, ("8.8.8.8", "53"))
   (reply, _,) = cs.recvfrom(512)
-    
+   
   
   logger.log(DEBUG2, "our reply in full:") 
   logger.log(DEBUG2, hexdump(reply))
